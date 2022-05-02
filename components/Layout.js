5b12e15e5d6c1747aa6a8847056206d23d5dde36
Mohamed <maeid.kh@gmail.com>
2022-05-02 02:52:45 +0000

    -

--- components/Layout.js
+++ components/Layout.js
@@ -59,7 +59,7 @@ const ScrollTop = () => {
 		<button
 			className={`${
 				!showScrollTop && "hidden"
-			} fixed w-12 h-12 p-2 transition-transform border rounded-full border-secondary bg-primary/30 backdrop-blur-sm text-secondary right-8 bottom-8 hover:scale-95`}
+			} fixed w-12 h-12 p-2 transition-transform border rounded-full border-secondary bg-primary/30 backdrop-blur-sm text-secondary right-4 lg:right-8  bottom-4 lg:bottom-8 hover:scale-95`}
 			onClick={() => {
 				document.body.scrollIntoView({
 					behavior: "smooth",
